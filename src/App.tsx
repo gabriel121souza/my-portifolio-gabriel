90d3345246b8883eeafb4c0571ab5c422625871d
GABRIEL DE SOUZA RODRIGUES <46980103+gabriel121souza@users.noreply.github.com>
2025-07-01 20:06:10 -0300

    implements about

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Hero from './sections/Hero';
@@ -7,16 +6,15 @@ import About from './sections/About';
 import Skills from './sections/Skills';
 import Projects from './sections/Projects';
 import Contact from './sections/Contact';
-
 import './index.css';
 
 function App() {
   return (
-    <Router>
+       <Router>
       <div className="flex flex-col min-h-screen">
         <Header />
 
-        <main className="flex-grow pt-20"> {/* flex-grow para expandir e empurrar o footer */}
+        <main className="flex-grow"> {/* Removemos o padding-top aqui */}
           <Routes>
             <Route path="/" element={<Hero />} />
             <Route path="/about" element={<About />} />
@@ -26,9 +24,9 @@ function App() {
             <Route
               path="*"
               element={
-                <section className="text-center py-24 px-4">
-                  <h2 className="text-2xl font-bold">404 - Página Não Encontrada</h2>
-                  <p className="mt-2">
+                <section className="text-center py-12 md:py-24 px-4">
+                  <h2 className="text-2xl md:text-3xl font-bold">404 - Página Não Encontrada</h2>
+                  <p className="mt-2 text-sm md:text-base">
                     Parece que você se perdeu no espaço. Volte para o{' '}
                     <Link to="/" className="text-blue-500 hover:underline">Início</Link>.
                   </p>
